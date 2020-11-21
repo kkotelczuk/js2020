b884147c9a1c365315139d130bf3fdc09ffee052
kkotelczuk <konrad.kotelczuk@gmail.com>
2020-11-21 22:34:23 +0100

    initial

---
+++ w1/r.jsx
@@ -0,0 +1,19 @@
+renderMyComponent = () => {
+  const someData = [1, 2, 3, 4, 5, 6];
+  return (
+    <div>
+      {someData.map(item => (
+        <span>{item}</span>
+      ))}
+    </div>
+  );
+};
+
+function greet(input) {
+  return input != null ? input : "Hello world";
+}
+
+// ES2020 nullish coalescing
+function greet(input) {
+  return input ?? "Hello world";
+}
